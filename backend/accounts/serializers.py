e7f9ab66778fd8ba70d9a9625c671835d91a39b5
RebecaNedelcu <nedelcu.rebeca@gmail.com>
2021-08-31 12:15:20 +0300

    contact

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -5,11 +5,19 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        help_text='Leave empty if no change needed',
+        style={'input_type': 'password', 'placeholder': 'Password'}
+    )
 
     def create(self, validated_data):
         user = User.objects.create(
-            username=validated_data['username'],
+            username=validated_data['email'],
             email=validated_data['email'],
+            first_name = validated_data['first_name'],
+            last_name = validated_data['last_name'],
         )
 
         user.set_password(validated_data['password'])
@@ -22,6 +30,7 @@ class UserSerializer(serializers.ModelSerializer):
         fields = [
             'id',
             'username',
+            'password',
             'email',
             'is_staff',
             'is_active',
